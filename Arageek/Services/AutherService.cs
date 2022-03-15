407a77ec1eb6aae0b8f39f3b97c9abeae4edbace
Ahmed Salah Abd-Elgawad <93048544+ahmedsalahaly@users.noreply.github.com>
2022-03-15 18:21:52 +0200

    AutherEdit

---
+++ Arageek/Services/AutherService.cs
@@ -0,0 +1,57 @@
+﻿using Arageek.Models;
+using Arageek.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arageek.Services
+{
+    public class AutherService : IAuther
+    {
+        ArageekDbContext context = new ArageekDbContext();
+        public void Add(Auther entity)
+        {
+            entity.CreatedDate = DateTime.Now;
+            context.authers.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Delete(int Id)
+        {
+            context.authers.Remove(Get(Id));
+            context.SaveChanges();
+        }
+
+        public Auther Get(int Id)
+        {
+            return context.authers.Find(Id);
+        }
+
+        public List<Auther> Get()
+        {
+            return context.authers.ToList();
+        }
+
+        public bool IsExist(Auther Name)
+        {
+            return context.authers.Any(x => x.Id == Name.Id);
+        }
+
+        public bool IsExistById(int Id)
+        {
+            return context.authers.Any(x => x.Id == Id);
+        }
+
+        public void Update(Auther entity)
+        {
+            Auther OldAuther = Get(entity.Id);
+            OldAuther.FirstName = entity.FirstName;
+            OldAuther.LastName = entity.LastName;
+            OldAuther.BirthDay = entity.BirthDay;
+            context.authers.Update(OldAuther);
+            context.SaveChanges();
+        }
+    }
+}
